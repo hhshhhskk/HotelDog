a060bcebf378f1764050410b4e55e31d0fb1411f
Umingoo <uming0097@gmail.com>
2024-01-25 09:39:46 +0900

    Design : 마이페이지 퍼블리싱 수정

--- src/components/MyPage/Booking.js
+++ src/components/MyPage/Booking.js
@@ -18,7 +18,7 @@ const PageTitle = styled.div`
     color: #654222;
   }
 `;
-const BookingList = styled.div`
+const BookingListNone = styled.div`
   position: relative;
   display: flex;
   width: 950px;
@@ -62,6 +62,97 @@ const BookingList = styled.div`
     align-items: center;
   }
 `;
+const BookingList = styled.div`
+  position: relative;
+`;
+const BookingContents = styled.div`
+  position: relative;
+  display: flex;
+`;
+const BookingRight = styled.div`
+  position: relative;
+`;
+const BookingLeft = styled.div`
+  position: relative;
+`;
+const BookDateBox = styled.div`
+  position: relative;
+  display: flex;
+`;
+const BoxLeft = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* text-align: center; */
+  width: 150px;
+  height: 70px;
+  border-radius: 10px 0px 0px 10px;
+  border: 1px solid #654222;
+  background: #fff;
+  p {
+    font-size: 16px;
+    font-weight: 700;
+    color: #654222;
+  }
+`;
+const BoxRight = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* text-align: center; */
+  width: 150px;
+  height: 70px;
+  border-radius: 0px 10px 10px 0px;
+  border: 1px solid #654222;
+  border-left: none;
+  background: #fff;
+  p {
+    font-size: 16px;
+    font-weight: 700;
+    color: #654222;
+  }
+`;
+const BookInfo = styled.div`
+  position: relative;
+  display: flex;
+  border-left: 3px solid #654222;
+  span {
+    margin-left: 10px;
+
+    font-size: 16px;
+    font-weight: 600;
+    color: #654222;
+  }
+`;
+const BookInfoTxt = styled.div`
+  position: relative;
+  margin-left: 17px;
+  p {
+    font-size: 14px;
+    color: #9d9d9d;
+  }
+`;
+const PayInfo = styled.div`
+  position: relative;
+  display: flex;
+  span {
+    font-size: 16px;
+    font-weight: 600;
+    color: #654222;
+  }
+`;
+const PayInfoTxt = styled.div`
+  position: relative;
+  margin-left: 17px;
+  p {
+    font-size: 14px;
+    color: #9d9d9d;
+  }
+`;
 
 const Booking = () => {
   return (
@@ -69,11 +160,50 @@ const Booking = () => {
       <PageTitle>
         <p>예약 내역</p>
       </PageTitle>
-      <BookingList>
+      {/* <BookingListNone>
         <img src={`${process.env.PUBLIC_URL}/images/MyPage/booking.svg`} />
         <p>예약 내역이 없습니다.</p>
         <span>다양한 호텔상품을 만나보세요</span>
         <a href="/"> 호텔 목록 보러가기</a>
+      </BookingListNone> */}
+      <BookingList>
+        <p>교동 쉽독호텔&리조트</p>
+        <span>대구광역시 중구</span>
+        <BookingContents>
+          <BookingLeft>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/MyPage/example_booking.svg`}
+            />
+          </BookingLeft>
+          <BookingRight>
+            <BookDateBox>
+              <BoxLeft>
+                <span>체크인</span>
+                <p>23.01.01</p>
+              </BoxLeft>
+              <BoxRight>
+                <span>체크아웃</span>
+                <p>23.01.01</p>
+              </BoxRight>
+            </BookDateBox>
+            <BookInfo>
+              <span>예약 정보</span>
+              <BookInfoTxt>
+                <p>예약 번호 : 2591564896</p>
+                <p>객실 : 2호 개인실</p>
+                <p>강아지 : 콩이</p>
+              </BookInfoTxt>
+            </BookInfo>
+            <PayInfo>
+              <span>결제 정보</span>
+              <PayInfoTxt>
+                <p>결제 금액 : 45,000원</p>
+                <p>결제 수단 : 카카오페이</p>
+              </PayInfoTxt>
+            </PayInfo>
+            <button>예약취소</button>
+          </BookingRight>
+        </BookingContents>
       </BookingList>
     </BookingPage>
   );
