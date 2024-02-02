d32a68af8a59022e0c3b68fc453256bd905174ef
rillyyy <https://github.com/rillyyy/react-study.git>
2024-02-02 17:08:48 +0900

    feat : axios 작업 중

---
+++ src/api/Detail/hoteldetailApi.js
@@ -0,0 +1,60 @@
+import jwtAxios from "../../utils/jwtUtil";
+const API_SERVER_URL = "";
+const prefix = `${API_SERVER_URL}`;
+
+// 호텔 전체 가져오기(get)
+export const getHotel = async (hotel_pk, setHotelList) => {
+  try {
+    console.log("getHotel hotel_pk: ", hotel_pk);
+    const url = `${prefix}/api/hotel?hotel_pk=${hotel_pk}`;
+    // http://112.222.157.156:5222/api/reservation?userPk=0
+    const res = await jwtAxios.get(url);
+    const resStatus = res.status.toString();
+    // 정상이라면
+    if (resStatus.charAt(0) === "2") {
+      setHotelList([...res.data]);
+    }
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+// 호텔 한개만 가져오기
+export const getHotelId = async (hotel_pk, successGetHotelId) => {
+  try {
+    console.log("getHotelId hotel_pk: ", hotel_pk);
+    const url = `${prefix}/api/hotel?hotel_pk=${hotel_pk}`;
+    // http://112.222.157.156:5222/api/reservation?userPk=0
+    const res = await jwtAxios.get(url);
+    const resStatus = res.status.toString();
+    // 정상이라면
+    if (resStatus.charAt(0) === "2") {
+      successGetHotelId(res.data);
+    }
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+    alert(` ${error} 가 발생했습니다. `);
+    window.location.href = "/errorpage";
+  }
+};
+
+// 예약 등록(post)
+export const postReservation = async userPk => {
+  try {
+    const url = `${prefix}/api/reservation?userPk=${userPk}`;
+    // http://112.222.157.156:5222/api/reservation?userPk=0
+    const res = await jwtAxios.post(url);
+    const resStatus = res.status.toString();
+  } catch (error) {
+    console.log(error);
+  }
+};
+// 예약 삭제(delete)
+export const deleteReservation = async () => {
+  try {
+    const res = await jwtAxios.delete(API_SERVER_URL);
+  } catch (error) {
+    console.log(error);
+  }
+};
